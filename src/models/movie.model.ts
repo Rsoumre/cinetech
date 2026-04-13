572b218502d10af5c360092b643952d8a1485655
Rsoumre <harounasoumare547@gmail.com>
2026-04-13 10:49:56 +0200

    add TVShow interface model

--- src/models/movie.model.ts
+++ src/models/movie.model.ts
@@ -3,9 +3,9 @@ export interface Movie {
     title: string;
     original_title: string;
     overview: string;
-    posteur_path: string;
+    poster_path: string;
     backdrop_path: string;
-    relaese_date: string;
+    release_date: string;
     vote_average: number;
     genre_ids: number[];
     popularity: number;
---
+++ src/models/tv.model.ts
@@ -0,0 +1,12 @@
+export interface TVSHOW {
+    id: number;
+    name: string;
+    original_name: string;
+    overview: string;
+    poster_path: string;
+    backdrop_path: string;
+    first_air_date: string;
+    vote_average: number;
+    genre_ids: number[];
+    popularity: number;
+}
